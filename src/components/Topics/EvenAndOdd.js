e6d53cf2e5043c7ab94cae7873af7676dfe86394
Sarah Drew <drew.sarah.e@gmail.com>
2021-09-23 00:02:00 -0700

    made numberSorter function

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,10 +1,40 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const EvenAndOdd = () => {
-  const evenArray = [];
-  const oddArray = [];
-  const [userInput, changeUserInput] = useState("");
+  const [evenArray, setEvenArray] = useState([]);
+  const [oddArray, setOddArray] = useState([]);
+  const [userInput, setUserInput] = useState("");
+
+  console.log({ evenArray });
+
+  // useEffect(() => {
+  //   if (userInput.length > 5) {
+  //     console.log(userInput);
+  //   }
+  // }, [userInput]);
+
+  const numberSorter = (input) => {
+    const numbers = input.split(", ");
+    const evenArr = [];
+    const oddArr = [];
+
+    numbers.forEach((element) => {
+      const number = parseInt(element);
+
+      if (isNaN(number)) {
+        alert("idiot, that doesn't contain all numbers!");
+        return;
+      }
+      if (number % 2 === 0) {
+        evenArr.push(number);
+      } else {
+        oddArr.push(number);
+      }
+    });
+
+    setEvenArray(evenArr);
+    console.log(evenArr);
+  };
 
   return (
     <div className="puzzleBox evenAndOddPB">
@@ -13,9 +43,13 @@ const EvenAndOdd = () => {
         type="text"
         className="inputLine"
         value={userInput}
-        onChange={(evt) => changeUserInput(evt.target.value)}
+        onChange={({ target }) => {
+          setUserInput(target.value);
+        }}
       />
-      <button className=""></button>
+      <button className="" onClick={() => numberSorter(userInput)}>
+        Split
+      </button>
       <span className="resultsBox"></span>
       <span className="resultsBox"></span>
     </div>
