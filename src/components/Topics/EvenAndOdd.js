1efd7be4769e3e191339d1ee70f73a03ad2dac62
Sarah Drew <drew.sarah.e@gmail.com>
2021-09-22 21:03:24 -0700

    add state for user input

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { useState } from "react";
 
 const EvenAndOdd = () => {
   const evenArray = [];
   const oddArray = [];
-  let userInput = "";
+  const [userInput, changeUserInput] = useState("");
 
   return (
     <div className="puzzleBox evenAndOddPB">
       <h4>Evens and Odds</h4>
-      <input type="text" className="inputLine" />
+      <input
+        type="text"
+        className="inputLine"
+        value={userInput}
+        onChange={(evt) => changeUserInput(evt.target.value)}
+      />
       <button className=""></button>
       <span className="resultsBox"></span>
       <span className="resultsBox"></span>
