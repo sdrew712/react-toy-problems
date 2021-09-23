a8e82b07486b39eec48b57b534e0d92564fec477
Sarah Drew <drew.sarah.e@gmail.com>
2021-09-23 13:25:35 -0700

    add palindrome html/state, checkPalindrome

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Palindrome = () => {
+  const [userInput, setUserInput] = useState("");
+  const [palindrome, setPalindrome] = useState("");
+
+  const checkPalindrome = (input) => {
+    let isPalindrome = false;
+
+    //create array of inputted text
+    const normalStringArr = input.toLowerCase().split("");
+
+    //create reversed array of inputted text
+    const reversedStringArr = [...normalStringArr].reverse();
+
+    //make the two arrays strings to compare
+    const normalString = normalStringArr.join();
+    const reversedString = reversedStringArr.join();
+
+    //if they are equal then it is a palindrome
+    if (normalString === reversedString) {
+      isPalindrome = true;
+    }
+
+    setPalindrome(JSON.stringify(isPalindrome));
+  };
+
   return (
-    <div>
-      <p>Palindrome</p>
+    <div className="puzzleBox filterStringPB">
+      <h4>Palindrome</h4>
+      <input
+        type="text"
+        className="inputLine"
+        value={userInput}
+        onChange={({ target }) => {
+          setUserInput(target.value);
+        }}
+      />
+      <button
+        className="confirmationButton"
+        onClick={() => checkPalindrome(userInput)}
+      >
+        Check
+      </button>
+      <span className="resultsBox">{"Palindrome: " + palindrome}</span>
     </div>
   );
 };
