8267ffc27f3c0c5ffb997d96d8adf25534fe9425
Sarah Drew <drew.sarah.e@gmail.com>
2021-09-23 13:50:54 -0700

    add sum, initial state

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Sum = () => {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [sum, setSum] = useState(null);
+
   return (
-    <div>
-      <p>Sum</p>
+    <div className="puzzleBox sumPB">
+      <h4>Sum</h4>
+      <input type="text" className="inputLine" />
+      <input type="text" className="inputLine" />
+      <button className="confirmationButton"></button>
+      <span className="resultsBox"></span>
     </div>
   );
 };
