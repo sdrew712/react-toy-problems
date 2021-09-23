92244b1c3764abe1b77bc0ab14bd96cfc6ee2464
Sarah Drew <drew.sarah.e@gmail.com>
2021-09-23 15:00:20 -0700

    move components to TopicBrowser

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from "react";
 import TopicBrowser from "./components/TopicBrowser/TopicBrowser";
-import EvenAndOdd from "./components/Topics/EvenAndOdd";
-import FilterObject from "./components/Topics/FilterObject";
-import FilterString from "./components/Topics/FilterString";
-import Palindrome from "./components/Topics/Palindrome";
-import Sum from "./components/Topics/Sum";
 
 const App = () => {
   return (
     <div>
       <TopicBrowser />
-      <EvenAndOdd />
-      <FilterObject />
-      <FilterString />
-      <Palindrome />
-      <Sum />
     </div>
   );
 };
--- src/components/TopicBrowser/TopicBrowser.js
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -1,9 +1,18 @@
 import React from "react";
+import EvenAndOdd from "../Topics/EvenAndOdd";
+import FilterObject from "../Topics/FilterObject";
+import FilterString from "../Topics/FilterString";
+import Palindrome from "../Topics/Palindrome";
+import Sum from "../Topics/Sum";
 
 const TopicBrowser = () => {
   return (
     <div>
-      <p>TopicBrowser</p>
+      <EvenAndOdd />
+      <FilterObject />
+      <FilterString />
+      <Palindrome />
+      <Sum />
     </div>
   );
 };
